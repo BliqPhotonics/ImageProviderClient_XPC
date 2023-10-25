ac3cc8abdfb28ab08f23fa8b93c0fffbe1a87533
Steve Begin <steve@bliqphotonics.com>
2023-10-25 08:50:33 -0400

    Initial Commit

---
+++ ImageProviderClient_XPC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageProviderClient_XPC
+//
+//  Created by Steve Begin on 2023-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageProviderClient_XPC/ImageProviderClient_XPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProviderClient_XPCApp.swift
+//  ImageProviderClient_XPC
+//
+//  Created by Steve Begin on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct ImageProviderClient_XPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
